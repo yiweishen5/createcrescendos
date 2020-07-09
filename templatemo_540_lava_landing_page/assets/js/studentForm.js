5605d4114a22d277459139aabce7339f7dcafdcf
zach <zachtang0123@gmail.com>
2020-07-09 00:06:38 -0500

    matched mentors show on student form now

--- templatemo_540_lava_landing_page/assets/js/studentForm.js
+++ templatemo_540_lava_landing_page/assets/js/studentForm.js
@@ -2,8 +2,6 @@ let studentForm = document.querySelector('#studentForm');
 let instrumentInput = document.querySelector('#instrumentInput').options;
 let purposeInput = document.querySelector('#purposeInput').options;
 
-const sessionStorage = window.sessionStorage;
-
 let mentors = [
     {
         name: 'Rbeca Collins',
@@ -59,13 +57,56 @@ studentForm.addEventListener('submit', (e) => {
         mentorMatches.push(mentor);     
     }
 
-    sessionStorage.setItem('numMentors', mentorMatches.length);
     mentorMatches.forEach((mentor, index) => {
         // console.log(index);
-        // console.log(JSON.stringify(mentor));
-        sessionStorage.setItem(`mentor${index}`, JSON.stringify(mentor));
-    });
+         
+        const nameText = document.createTextNode(mentor.name);
+        const name = document.createElement('p');
+        name.appendChild(nameText);
 
-    window.location.href = './matchedMentors.html';
+        const pic = document.createElement('img');
+        pic.src = mentor.picSrc;
+        pic.alt = mentor.name;
+
+        const bioText = document.createTextNode(mentor.bio);
+        const bio = document.createElement('p');
+        bio.appendChild(bioText);
+
+        const instruments = document.createElement('ul');
+        const teaches = document.createElement('ul');
+
+        const button = document.createElement('button');
+        button.innerHTML = `Connect with ${mentor.name}`;
+
+        for(let i = 0; i < mentor.instruments.length; ++i){
+            const instrumentText = document.createTextNode(mentor.instruments[i]);
+            const instrument = document.createElement('li');
+            instrument.appendChild(instrumentText);
+            
+            instruments.appendChild(instrument);
+        }
+
+        for(let i = 0; i < mentor.teaches.length; ++i){
+            const teachText = document.createTextNode(mentor.teaches[i]);
+            const teach = document.createElement('li');
+            teach.appendChild(teachText);
+
+            teaches.appendChild(teach);
+        }
+
+        let mentorCard = document.createElement('div');
+        mentorCard.className = 'mentorCard';
+
+        mentorCard.appendChild(name);
+        mentorCard.appendChild(pic);
+        mentorCard.appendChild(bio);
+        mentorCard.appendChild(instruments);
+        mentorCard.appendChild(teaches);
+        mentorCard.appendChild(button);
+
+        console.log(bio);
+
+        document.body.appendChild(mentorCard);
+    });
 });
 
